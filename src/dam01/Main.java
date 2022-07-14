5bfdfaf33257111a2c392e5d14a2c29e03588d36
Sergi <sergivi04@gmail.com>
2022-07-14 14:07:49 +0200

    rollPins v1

--- src/dam01/Main.java
+++ src/dam01/Main.java
@@ -1,8 +1,46 @@
 package dam01;
 
+import dam01.Models.Game;
+
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Holaa");
+        Main main = new Main();
+        main.inici();
+    }
+
+    public void inici(){
+        Game actualGame = newGame();
+        actualGame.roll(rollPins(actualGame));
+    }
+
+
+    public Game newGame(){
+        return new Game();
+    }
+    public int rollPins(Game actualGame){
+        int pinsDown = 0;
+        for(int i = 0; i < 2; i++){
+            pinsDown = randomNumberGenerator(1, actualGame.getPins());
+            comprovatePins(pinsDown, actualGame);
+            actualGame.setPins(10 - pinsDown);
+            System.out.println(pinsDown);
+        }
+
+        return pinsDown;
+    }
+    public void comprovatePins(int pinsDown, Game actualGame){
+        if (pinsDown == 10){
+            actualGame.setPins(10);
+            rollPins(actualGame);
+        }
+
+    }
+
+    public int randomNumberGenerator(int min, int max){
+        Random rand = new Random();
+        return rand.nextInt((max - min) + 1) + min;
     }
 }
--- src/dam01/Models/Game.java
+++ src/dam01/Models/Game.java
@@ -1,13 +1,36 @@
 package dam01.Models;
 
 public class Game {
-    
+    private int  pins;
+    private int score;
+    private boolean isStrike;
+    private boolean isSpare;
+
+    public Game() {
+        this.pins = 10;
+        this.score = 0;
+    }
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getPins() {
+        return pins;
+    }
+
+    public void setPins(int pins) {
+        this.pins = pins;
+    }
+
+    public int getScore() {
+        return score;
+    }
 
     public void roll(int pinsDown){
 
     }
     public int score(){
-
+        int finalScore=0;
         return finalScore;
     }
 }
